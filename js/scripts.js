9ebefe38635a4880f42fb3c7940fa32085c2d8ce
Jasmine Lee and Shawn Lunsford <shawn@>
2019-01-23 10:23:52 -0800

    did everything and helped stuff

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#button30").click(function(){
+    for (var i = 5; i <= 30; i += 5){
+      alert(i)
+  }
+  });
+  $("#button50").click(function(){
+    for (var i = 7; i <= 50; i += 7){
+      alert(i)
+  }
+  });
+});
